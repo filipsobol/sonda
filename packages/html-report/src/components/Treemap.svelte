3d8bee5e010dc90b531cb09b9671b00114208ceb
Filip Sobol <filipsobol@users.noreply.github.com>
2024-09-17 00:21:13 +0200

    Always re-render Treemap component when path, width, or height change

--- packages/html-report/src/components/Treemap.svelte
+++ packages/html-report/src/components/Treemap.svelte
@@ -1,19 +1,22 @@
-<svg
-	width={ width }
-	height={ height }
-	xmlns="http://www.w3.org/2000/svg"
-	role="img"
->
-	<!-- Substract 1 and start at 0.5 to prevent stroke clipping at viewport edges -->
-	<Level
-		content={ content }
-		totalBytes={ content.bytes }
-		width={ width - 1 }
-		height={ height - 1 }
-		xStart={ 0.5 }
-		yStart={ 0.5 }
-	/>
-</svg>
+<!-- Re-render the entire SVG when path, width, or height change -->
+{#key [ content.path, width, height ]}
+	<svg
+		width={ width }
+		height={ height }
+		xmlns="http://www.w3.org/2000/svg"
+		role="img"
+	>
+		<!-- Substract 1 and start at 0.5 to prevent stroke clipping at viewport edges -->
+		<Level
+			content={ content }
+			totalBytes={ content.bytes }
+			width={ width - 1 }
+			height={ height - 1 }
+			xStart={ 0.5 }
+			yStart={ 0.5 }
+		/>
+	</svg>
+{/key}
 
 <script lang="ts">
 import Level from './Level.svelte';
