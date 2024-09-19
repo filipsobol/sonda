7fefa44f664e4af5eb49ba79c2d8fdbc9baeb9d3
Filip Sobol <filipsobol@users.noreply.github.com>
2024-09-19 23:27:50 +0200

    Remove `upath` and transform paths to POSIX format

--- packages/sonda/src/utils.ts
+++ packages/sonda/src/utils.ts
@@ -1,24 +1,20 @@
-import { relative } from 'path';
+import { relative, posix, sep } from 'path';
 import { exec } from 'child_process';
-import upath from 'upath';
-
-const normalizers: Array<[ RegExp, string ]> = /* #__PURE__ */ ( () => [
-	// Unicode escape sequences used by Rollup and Vite to identify virtual modules
-	[ /^\0/, '' ],
-] )();
 
 const cwd = process.cwd();
 
 export function normalizePath( path: string ): string {
-	const normalized = normalizers.reduce(
-		( p, [ pattern, replace ] ) => p.replace( pattern, replace ),
-		upath.normalizeSafe( path )
-	);
+	// Unicode escape sequences used by Rollup and Vite to identify virtual modules
+	const normalized = path.replace( /^\0/, '' )
 
-	return relative( cwd, normalized );
+	// Transform absolute paths to relative paths
+	const relativized = relative( cwd, normalized );
+
+	// Ensure paths are POSIX-compliant - https://stackoverflow.com/a/63251716/4617687
+	return relativized.replaceAll( sep, posix.sep );
 }
 
-function getCommandLine() {
+function getOpenCommand() {
 	switch ( process.platform ) {
 		case 'darwin': return 'open';
 		case 'win32': return 'start';
@@ -27,5 +23,5 @@ function getCommandLine() {
 }
 
 export function open( path: string ): void {
-	exec( getCommandLine() + ' ' + path )
+	exec( getOpenCommand() + ' ' + path )
 }
