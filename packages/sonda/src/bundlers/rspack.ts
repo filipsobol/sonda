d981f1b8ec07a20d71afb62e6c8b67602e698495
Filip Sobol <filipsobol@users.noreply.github.com>
2024-10-10 21:14:53 +0200

    Use `stats` object in rspack integration

--- packages/sonda/src/bundlers/rspack.ts
+++ packages/sonda/src/bundlers/rspack.ts
@@ -1,92 +1,69 @@
 import { join } from 'path';
 import { normalizeOptions, normalizePath } from '../utils';
 import { generateReportFromAssets } from '../report/generate';
+import type { Compiler, StatsModule } from '@rspack/core';
 import type { Options, ModuleFormat, JsonReport } from '../types';
-import { NormalModule, type Compiler, type Module } from 'webpack';
 
 const jsRegexp = /\.[c|m]?[t|j]s[x]?$/;
 
 export class SondaRspackPlugin {
 	options: Partial<Options>;
-	inputs: JsonReport[ 'inputs' ];
 
 	constructor ( options?: Partial<Options> ) {
 		this.options = options || {};
-		this.inputs = {};
 	}
 
 	apply( compiler: Compiler ): void {
 		compiler.options.output.devtoolModuleFilenameTemplate = '[absolute-resource-path]';
 
-		const importsMap = new Map<string, string[]>();
-
-		compiler.hooks.compilation.tap( 'SondaWebpackPlugin', ( compilation, { normalModuleFactory } ) => {
-			normalModuleFactory.hooks.afterResolve.tap( 'SondaWebpackPlugin', resolveData => {
-				const importer = resolveData.contextInfo.issuer;
-				const imported = resolveData.createData.resource;
-
-				if ( !importer || !imported ) {
-					return;
-				}
-
-				if ( !importsMap.has( importer ) ) {
-					importsMap.set( importer, [] );
-				}
-
-				const imports = importsMap.get( importer )!;
-
-				imports.push( imported );
-				importsMap.set( importer, imports );
-			} );
-
-			compilation.hooks.optimizeModules.tap( 'SondaWebpackPlugin', ( modules ) => {
-				Array
-					.from( modules )
-					.forEach( module => {
-						if ( !isNormalModule( module ) ) {
-							return;
-						}
-
-						if ( !module.resource ) {
-							return;
-						}
-
-						this.inputs[ normalizePath( module.resource ) ] = {
-							bytes: module.size(),
-							format: getFormat( module ),
-							imports: importsMap.get( module.resource ) || [],
-							belongsTo: null,
-						};
-					} );
-			} );
-		} );
-
-		compiler.hooks.afterEmit.tapAsync( 'SondaWebpackPlugin', ( compilation, callback ) => {
+		compiler.hooks.afterEmit.tapPromise( 'SondaRspackPlugin', compilation => {
+			const inputs: JsonReport[ 'inputs' ] = {};
 			const outputPath = compiler.options.output.path || compiler.outputPath || process.cwd();
-			const assets = compilation.getAssets().map( asset => join( outputPath, asset.name ) );
+			const stats = compilation.getStats().toJson( { modules: true } );
 
-			generateReportFromAssets(
-				assets,
-				this.inputs,
+			const modules = stats.modules
+				?.filter( mod => !mod.codeGenerated )
+				.filter( mod => mod.nameForCondition )
+				|| [];
+
+			modules.forEach( module => {
+				const imports = modules.reduce( ( acc, { nameForCondition, issuerName, reasons } ) => {
+					if ( !nameForCondition ) {
+						return acc;
+					}
+
+					if ( issuerName === module.name || reasons?.some( reason => reason.resolvedModule === module.name ) ) {
+						acc.push( normalizePath( nameForCondition ) );
+					}
+
+					return acc;
+				}, [] as Array<string> );
+
+				inputs[ normalizePath( module.nameForCondition! ) ] = {
+					bytes: module.size,
+					format: getFormat( module ),
+					imports,
+					belongsTo: null
+				};
+			} );
+
+			return generateReportFromAssets(
+				stats.assets?.map( asset => join( outputPath, asset.name ) ) || [],
+				inputs,
 				normalizeOptions( this.options )
-			)
-				.then( () => callback() );
+			);
 		} );
 	}
 }
 
-function getFormat( module: NormalModule ): ModuleFormat {
-	if ( !jsRegexp.test( module.resource ) ) {
+function getFormat( module: StatsModule ): ModuleFormat {
+	if ( !jsRegexp.test( module.nameForCondition! ) ) {
 		return 'unknown';
 	}
 
-	if ( module.type === 'javascript/esm' || module.buildMeta?.exportsType === 'namespace' ) {
+	if ( module.moduleType === 'javascript/esm' ) {
 		return 'esm';
 	}
 
 	return 'cjs';
-}
-
-function isNormalModule( module: Module | NormalModule | null ): module is NormalModule {
-	return module !== null && 'resource' in module;
 }
