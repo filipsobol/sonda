5335e313e1bdcbff2b2a8f1c580be015785913a1
Filip Sobol <filipsobol@users.noreply.github.com>
2024-10-11 23:23:21 +0200

    Modify `SondaWebpackPlugin` to work in both webpack and rspack

--- playground/rspack/rsbuild.config.mjs
+++ playground/rspack/rsbuild.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from '@rsbuild/core';
-import { SondaRspackPlugin } from 'sonda';
+import { SondaWebpackPlugin } from 'sonda';
 
 export default defineConfig( {
 	tools: {
 		rspack: {
 			devtool: 'source-map',
 			plugins: [
-				new SondaRspackPlugin( {
+				new SondaWebpackPlugin( {
 					gzip: true,
 					brotli: true,
 					detailed: true
