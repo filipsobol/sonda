2d82de40951ee049404bde086dd346998e96b16a
Filip Sobol <filipsobol@users.noreply.github.com>
2025-03-01 19:27:15 +0100

    Update more dependencies

--- playground/vite/vite.config.js
+++ playground/vite/vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
-import virtual from 'vite-plugin-virtual'
 import Sonda from 'sonda/vite';
 
 export default defineConfig( {
   plugins: [
-    virtual( {
-      'virtual:module': `export default { hello: 'world' }`,
-      'virtual:config': { hello: 'world' }
-    } ),
     Sonda(),
   ],
   build: {
