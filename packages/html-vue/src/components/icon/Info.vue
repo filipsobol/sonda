207b97463b441b735ccb6745830af886af46d4f2
Filip Sobol <filipsobol@users.noreply.github.com>
2025-05-21 23:10:04 +0200

    Add Info icon

---
+++ packages/html-vue/src/components/icon/Info.vue
@@ -0,0 +1,11 @@
+<template>
+	<Icon>
+		<circle cx="12" cy="12" r="10"/>
+		<path d="M12 16v-4"/>
+		<path d="M12 8h.01"/>
+	</Icon>
+</template>
+
+<script setup lang="tsx">
+import Icon from './Icon.vue';
+</script>
