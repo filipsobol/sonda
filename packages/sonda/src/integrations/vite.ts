b5dce980b0bd54091876d8643e11e36f8ad91246
Filip Sobol <filipsobol@users.noreply.github.com>
2025-06-08 11:50:11 +0200

    Fix Dependency Tree in Vite

--- packages/sonda/src/integrations/vite.ts
+++ packages/sonda/src/integrations/vite.ts
@@ -14,6 +14,7 @@ export function SondaVitePlugin( userOptions: UserOptions = {} ): PluginOption {
   return {
     ...SondaRollupPlugin( options ),
     name: 'sonda-vite',
+    enforce: 'pre',
     apply: 'build'
   };
 }
