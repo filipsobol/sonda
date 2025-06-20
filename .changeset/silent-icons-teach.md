6d73d7929ecf937a67299cf32dbca8fa8fabc0ca
Filip Sobol <filipsobol@users.noreply.github.com>
2025-06-20 12:05:49 +0200

    Revert "Categorize ".json", ".node", and ".wasm" files as "other", not "script""
    This reverts commit 738e6e4a4e2ee40b9c7569b8368f03ec110b11e3.

--- .changeset/silent-icons-teach.md
+++
@@ -1,5 +0,0 @@
----
-"sonda": minor
----
-
-Categorize ".json", ".node", and ".wasm" files as "other", not "script"
--- packages/sonda/src/utils.ts
+++ packages/sonda/src/utils.ts
@@ -12,6 +12,9 @@ export const extensions: Record<string, FileType> = {
 	'.tsx': 'script',
 	'.cts': 'script',
 	'.mts': 'script',
+	'.json': 'script',
+	'.node': 'script',
+	'.wasm': 'script',
 
 	// Styles
 	'.css': 'style',
