7e9820cc87386b721961627e935d7dfd41d1f8fc
Filip Sobol <filipsobol@users.noreply.github.com>
2025-06-24 00:01:49 +0200

    Update og image URLs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,13 +9,12 @@ export default defineConfig({
   description: 'Universal bundle analyzer and visualizer. Works with Vite, Rollup, esbuild, webpack, Rolldown, Rspack, Next.js, Nuxt, Astro, SvelteKit and Angular CLI.',
   head: [
     [ 'link', { rel: 'icon', href: '/sonda.ico' } ],
-    [ 'meta', { property: 'og:url', content: URL } ],
     [ 'meta', { property: 'og:title', content: 'Sonda' } ],
     [ 'meta', { property: 'og:description', content: 'Universal bundle analyzer and visualizer that works with most popular bundlers and frameworks.' } ],
     [ 'meta', { property: 'og:type', content: 'website' } ],
-    [ 'meta', { property: 'og:image', content: '/og-image.png' } ],
+    [ 'meta', { property: 'og:image', content: URL + '/og-image.png' } ],
     [ 'meta', { name: 'twitter:card', content: 'summary_large_image' } ],
-    [ 'meta', { name: 'twitter:image', content: '/og-image.png' } ],
+    [ 'meta', { name: 'twitter:image', content: URL + '/og-image.png' } ],
   ],
   sitemap: {
     hostname: URL
