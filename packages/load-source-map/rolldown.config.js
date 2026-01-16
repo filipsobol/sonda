edd578e3694846a1f316aea8d6034573cee2545d
Filip Sobol <filipsobol@users.noreply.github.com>
2026-01-16 21:43:02 +0100

    Downgrade rolldown-plugin-dts to fix builds

--- packages/load-source-map/rolldown.config.js
+++ packages/load-source-map/rolldown.config.js
@@ -1,17 +1,12 @@
-import { rm } from 'fs/promises';
 import { builtinModules } from 'module';
 import { defineConfig } from 'rolldown';
 import { dts } from 'rolldown-plugin-dts';
-
-// Remove old build folder
-await rm('dist', { recursive: true, force: true });
 
 export default defineConfig({
 	output: {
 		dir: 'dist',
 		format: 'esm',
-		entryFileNames: '[name].js',
-		chunkFileNames: '[name].js'
+		cleanDir: true
 	},
 	input: 'src/index.ts',
 	platform: 'node',
--- packages/sonda/rolldown.config.js
+++ packages/sonda/rolldown.config.js
@@ -1,18 +1,13 @@
-import { rm } from 'fs/promises';
 import { builtinModules } from 'module';
 import { defineConfig } from 'rolldown';
 import { dts } from 'rolldown-plugin-dts';
 import pkg from './package.json' with { type: 'json' };
 
-// Remove old build folder
-await rm('dist', { recursive: true, force: true });
-
 export default defineConfig({
 	output: {
 		dir: 'dist',
 		format: 'esm',
-		entryFileNames: '[name].js',
-		chunkFileNames: '[name].js'
+		cleanDir: true
 	},
 	input: {
 		index: 'src/index.ts',
